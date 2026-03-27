c2c3dc71a3b7859f687f168e0f78eb05a072b0ba
Timothy <jollyp388@gmail.com>
2026-03-27 11:26:25 +0100

    email recen

--- src/models/Booking.js
+++ src/models/Booking.js
@@ -7,6 +7,11 @@ const bookingSchema = new mongoose.Schema(
       ref: "User",
       required: true,
     },
+    instructor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
     slot: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Availability",
