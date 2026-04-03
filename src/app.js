20b45bc4ca6fba90b573e41683020be4e2315556
Zoro388 <jollyp388@gmail.com>
2026-04-03 08:36:45 +0100

    update 12

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
   "http://localhost:5173",
-  "https://xdrivee.netlify.app",
+  "https://xandersdrive.netlify.app",
   process.env.FRONTEND_URL, // your deployed frontend
 ];
 
